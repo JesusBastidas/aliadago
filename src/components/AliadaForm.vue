a32bd6f9d9ed2870198415a04761084e3c367896
JesusBastidas <davidbastidascarvajal@gmail.com>
2022-03-17 19:27:09 -0500

    nuevo boton, metodo para get

--- src/components/AliadaForm.vue
+++ src/components/AliadaForm.vue
@@ -4,22 +4,51 @@
         <label for="user">Usuario</label>
         <input 
         type="email"
-        class="form-control rounded-pill border border-primary" 
+        ref="idUsuario"
+        class="form-control form-control-lg rounded-pill border border-primary" 
         name="user" 
         id="user" 
         aria-describedby="helpId" 
         placeholder="Introduce tu correo">
         <small id="helpId" class="form-text text-muted"></small>
     </div>
-    <button type="button" class="rounded-pill">Log in</button>
+    <button type="button" id="btn1" class="rounded-pill" @click="verCorreo">Verificar</button>
+    <button v-if="estado" type="button" id="btn2" class="rounded-pill ">Log in</button>
   </div>
 </template>
 
 <script>
+
 export default {
     name:"AliadaForm",
-    component: {}
-
+    component: {},
+    data() {
+      return {
+        estado:false
+      }
+    },
+    methods: {
+      verCorreo() {
+        const userId= this.$refs.idUsuario.value;
+        if (userId){
+          /* try {
+            const respuesta = await fetch(`${baseURL}/${userId}`);
+          if (!respuesta.ok) {
+           // colocar aqui el mensaje de error; ;
+           this.estado= false
+           return
+          }
+          const data = await res.json();
+          this.estado=true;
+          //aqui va lo de la contrase;a
+        } catch (err) {
+          this.estado= false
+          //"mensaje de error" = err.message;
+          return
+        } */
+        }
+    }
+  },
 }
 </script>
 
